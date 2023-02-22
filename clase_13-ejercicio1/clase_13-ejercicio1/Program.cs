de007cb7685858cf5e8b7ef252cfab3d54a14111
Miguel Juarez <mfjuaarez@gmail.com>
2023-02-21 21:06:47 -0300

    Se agrego la el ejercicio 1 de la clase 13

---
+++ clase_13-ejercicio1/clase_13-ejercicio1/Program.cs
@@ -0,0 +1,64 @@
+﻿using clase_13_ejercicio1.models;
+
+namespace clase_13_ejercicio1
+{
+    internal class Program
+    {
+        /*
+         Crear una clase Billetera que tenga las siguientes propiedades públicas del tipo entero:
+
+            BilletesDe10
+            BilletesDe20
+            BilletesDe50
+            BilletesDe100
+            BilletesDe200
+            BilletesDe500
+            BilletesDe1000
+
+        Agregar un método que se llame Total del tipo decimal,
+        y que devuelva el Importe Total en la billetera teniendo en cuenta 
+        la cantidad de billetes que se tenga de cada tipo.
+
+        Agregar un metodo que se llame Combinar,
+        que reciba como parámetro otra billetera y que devuelva una nueva Billetera 
+        con la suma o combinacion del dinero de ambas billeteras.  
+        Una vez combinadas las 2 billeteras (y creada la tercera), 
+        las 2 primeras billeteras deberan quedar en cero. (Sin billetes)
+         
+         */
+        static void Main(string[] args)
+        {
+            Billetera miBilletera = new Billetera()
+            {
+                BilletesDe10 = 17,
+                BilletesDe50 = 3,
+                BilletesDe100= 4,
+                BilletesDe500 = 2,
+                BilletesDe200 = 1,
+                BilletesDe1000 = 7
+            };
+
+            Billetera billeteraVieja = new Billetera()
+            {
+                BilletesDe10 = 5,
+                BilletesDe100 = 10,
+                BilletesDe1000 = 2,
+                BilletesDe500 = 1
+            };
+
+            Console.WriteLine("EN MI BILLETERA TENGO $"+miBilletera.Total());
+            Console.WriteLine("EN LA BILLETERA VIEJA TENGO $"+billeteraVieja.Total());
+
+            Billetera billeteraNueva = miBilletera.Combinar(miBilletera, billeteraVieja);
+
+            Console.WriteLine($"ME COMPRE UNA BILLETERA NUEVA Y SUMANDO TODAS LAS BILLETERAS TENGO ${billeteraNueva.Total()}");
+
+            Console.WriteLine($"SALDO BILLETERA: ${miBilletera.Total()}");
+            Console.WriteLine($"SALDO BILLETERA VIEJA: ${billeteraVieja.Total()}");
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ clase_13-ejercicio1/clase_13-ejercicio1/models/Billetera.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_13_ejercicio1.models
+{
+    internal class Billetera
+    {
+        public int BilletesDe10 { get; set; }
+        public int BilletesDe50 { get; set; }
+        public int BilletesDe100 { get; set; }
+        public int BilletesDe200 { get; set; }
+        public int BilletesDe500 { get; set; }
+        public int BilletesDe1000 { get; set; }
+
+
+        //Constructor 
+
+        public Billetera() 
+        {
+            BilletesDe10 = 0;
+            BilletesDe50 = 0;
+            BilletesDe100 = 0;
+            BilletesDe200 = 0;
+            BilletesDe500 = 0;
+            BilletesDe1000 = 0;
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0;
+            total += BilletesDe10*10;
+            total += BilletesDe50*50;
+            total += BilletesDe100*100;
+            total += BilletesDe200*200;
+            total += BilletesDe500*500;
+            total += BilletesDe1000*1000;
+            return total;
+        }
+
+        public Billetera Combinar(Billetera billetera1, Billetera billetera2)
+        {
+            Billetera nueva = new Billetera()
+            {
+                BilletesDe10 = billetera1.BilletesDe10 + billetera2.BilletesDe10,
+                BilletesDe50 = billetera1.BilletesDe50 + billetera2.BilletesDe50,
+                BilletesDe100 = billetera1.BilletesDe100 + billetera2.BilletesDe100,
+                BilletesDe200 = billetera2.BilletesDe200 + billetera2.BilletesDe200,
+                BilletesDe500 = billetera1.BilletesDe500 + billetera2.BilletesDe500,
+                BilletesDe1000 = billetera1.BilletesDe1000 + billetera2.BilletesDe1000,
+                
+            };
+            billetera1.BilletesDe10 = 0;
+            billetera1.BilletesDe50 = 0;
+            billetera1.BilletesDe100 = 0;
+            billetera1.BilletesDe200 = 0;
+            billetera1.BilletesDe500 = 0;
+            billetera1.BilletesDe1000 = 0;
+            billetera2.BilletesDe10 = 0;
+            billetera2.BilletesDe50 = 0;
+            billetera2.BilletesDe100 = 0;
+            billetera2.BilletesDe200 = 0;
+            billetera2.BilletesDe500 = 0;
+            billetera2.BilletesDe1000 = 0;
+
+            return nueva;
+        }
+    }
+}
