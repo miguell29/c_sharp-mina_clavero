afdeea20e24da416a941459eb28a25160da10987
Miguel Juarez <mfjuaarez@gmail.com>
2023-03-04 11:58:43 -0300

    Se completo el ejercicio de Constructor de clase y se agrego el archivo gitignore

---
+++ clase_14-ejercicio_cartas_españolas/clase_14-ejercicio_cartas_españolas/models/Baraja.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clase_14_ejercicio_cartas_españolas.models
+{
+    internal class Baraja
+    {
+        public { get; set; }
+        public Baraja() 
+        {
+            
+        }
+    }
+}
